6b6c2bf015065d69e794518300d8b168499ef432
nitendra <nitendra@Maa>
2017-06-22 16:07:33 +0100

    user similarity matrix scheduling updated

--- personalized/user_sim_matrix_calc.py
+++ personalized/user_sim_matrix_calc.py
@@ -10,18 +10,22 @@ import re
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from apscheduler.schedulers.blocking import BlockingScheduler
 
+sched = BlockingScheduler()
 
 #each profile is stored in one document either in local or database
 #document name must be profile_id
-all_profiles=[] #List is created to store all profiles
-profiles_id=[]
-for filename in glob.glob('profiles/*'): #include all the files from current directory
-    fin = open(filename,"r")  #open the file for reading
-    profiles_id.append(os.path.basename(filename)) #document name for future reference
-    all_profiles.append(fin.read()) #read full content of file and added to the client
-    fin.close() #close the file
-print("Number of profiles %d" % len(all_profiles)) #Total number of profiles
+all_profiles=set() #List is created to store all profiles
+profiles_id=set()
+access_df=pd.DataFrame()
+def load_profiles():
+    for filename in glob.glob('profiles/*'): #include all the files from current directory
+        fin = open(filename,"r")  #open the file for reading
+        profiles_id.add(os.path.basename(filename)) #document name for future reference
+        all_profiles.add(fin.read()) #read full content of file and added to the client
+        fin.close() #close the file
+#print("Number of profiles %d" % len(all_profiles)) #Total number of profiles
 
 
 from sklearn.feature_extraction.text import CountVectorizer
@@ -40,12 +44,26 @@ def lemma_tokenizer(text):
 
 
 # we can pass in the same preprocessing parameters
-tf_idfVector = TfidfVectorizer(stop_words="english",min_df =1,ngram_range=(1,1))#chosen n-gram of three words. It will produce phrases containing upto three words
-tf_idfMatrix= tf_idfVector.fit_transform(all_profiles)
-cosSim=cosine_similarity(tf_idfMatrix)
-df = pd.DataFrame(cosSim,columns=profiles_id,index=profiles_id)
-print(df)
-print(df['110273156'].argmax())
+def calc_sim():
+    tf_idfVector = TfidfVectorizer(stop_words="english",min_df =1,ngram_range=(1,1))#chosen n-gram of three words. It will produce phrases containing upto three words
+    tf_idfMatrix= tf_idfVector.fit_transform(all_profiles)
+    cosSim=cosine_similarity(tf_idfMatrix)
+    df = pd.DataFrame(cosSim,columns=profiles_id,index=profiles_id)
+    #print(df['110273156'].argmax())
+    access_df=df
+    df=pd.DataFrame()
+
+
+@sched.scheduled_job('interval', seconds=30)
+def job_scheduler():
+    load_profiles()
+
+
+@sched.scheduled_job('interval', seconds=50)
+def job_scheduler():
+    if all_profiles is not None:
+        calc_sim()
 #print(df.to_dict())
 #print( df.groupby('110273156').head(2))
 
+sched.start()
